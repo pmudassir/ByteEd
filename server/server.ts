9fccd28c124158dccf2ad5ae8c3f1f649527fcca
pmudassir <pmuddasir@gmail.com>
2023-09-11 21:35:48 +0530

    initalised

---
+++ server/server.ts
@@ -0,0 +1,7 @@
+import { app } from "./app";
+require('dotenv').config()
+
+// Create Server
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+})
