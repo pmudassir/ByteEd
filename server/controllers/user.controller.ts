1c462f74c47cd72412cebdd815d895154068924c
pmudassir <pmuddasir@gmail.com>
2023-09-16 16:51:47 +0530

    working on user crud

---
+++ server/controllers/user.controller.ts
@@ -0,0 +1,60 @@
+require("dotenv").config()
+import { Request, Response, NextFunction } from "express";
+import userModel, { IUser } from "../models/User";
+import ErrorHandler from "../utils/ErrorHandler";
+import { CatchAsyncError } from "../middleware/catchAsyncErrors";
+import jwt, { Secret } from "jsonwebtoken"
+import ejs from "ejs"
+import path from "path";
+
+// user reg
+interface IRegistrationBody {
+    name: string
+    email: string
+    password: string
+    avatar?: string
+}
+
+export const userRegistration = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { name, email, password } = req.body
+
+        const emailExist = await userModel.findOne({ email })
+        if (emailExist) {
+            return next(new ErrorHandler("Email already exists", 400))
+        }
+
+        const user: IRegistrationBody = {
+            name,
+            email,
+            password
+        }
+
+        const activationToken = createActivationToken(user)
+        const activationCode = activationToken.activationCode
+
+        const data = { user: { name: user.name }, activationCode }
+
+        const html = await ejs.renderFile(path.join(__dirname, ""))
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 400))
+    }
+})
+
+interface IActivationToken {
+    token: string
+    activationCode: string
+}
+
+export const createActivationToken = (user: any): IActivationToken => {
+    const activationCode = Math.floor(1000 + Math.random() * 9000).toString()
+
+    const token = jwt.sign({
+        user,
+        activationCode
+    }, process.env.ACTIVATION_SECRET as Secret,
+        {
+            expiresIn: "5m"
+        })
+    return { token, activationCode }
+}
