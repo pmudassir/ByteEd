ae01a3180a58ece61f63ecb51e944bc966e590bd
pmudassir <pmuddasir@gmail.com>
2023-09-17 20:15:48 +0530

    mail sending function

--- server/utils/sendMail.ts
+++ server/utils/sendMail.ts
@@ -10,5 +10,32 @@ interface EmailOptions {
 }
 
 const sendMail = async (options: EmailOptions): Promise<void> => {
+    const transporter: Transporter = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: Number(process.env.SMTP_PORT),
+        service: process.env.SMTP_SERVICE,
+        auth: {
+            user: process.env.SMTP_MAIL,
+            pass: process.env.SMTP_PASSWORD
+        }
+    })
 
+    const { email, subject, template, data } = options
+
+    // path to email template file
+    const templatePath = path.join(__dirname, "../mails", template)
+
+    // render the email template with ejs
+    const html: string = await ejs.renderFile(templatePath, data)
+
+    const mailOptions = {
+        from: process.env.SMTP_MAIL,
+        to: email,
+        subject,
+        html
+    }
+
+    await transporter.sendMail(mailOptions)
 }
+
+export default sendMail
