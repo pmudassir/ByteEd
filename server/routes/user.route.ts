c19be2113bb06c39f413f059f541061f12f36a4a
pmudassir <pmuddasir@gmail.com>
2023-09-19 11:28:27 +0530

    user activation completed

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,7 +1,8 @@
 import express from "express"
-import { userRegistration } from "../controllers/user.controller"
+import { activateUser, userRegistration } from "../controllers/user.controller"
 const router = express.Router();
 
 router.post("/registration", userRegistration)
+router.post("/activate-user", activateUser)
 
 module.exports = router;
