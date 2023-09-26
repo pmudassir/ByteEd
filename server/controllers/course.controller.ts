507177493fe714ebff997b2f4038bb49beee7bb2
pmudassir <pmuddasir@gmail.com>
2023-09-26 23:14:32 +0530

    course in redis

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -4,6 +4,7 @@ import ErrorHandler from "../utils/ErrorHandler";
 import cloudinary from "cloudinary"
 import { createCourse } from "../services/course.services";
 import CourseModel from "../models/Course";
+import { redis } from "../utils/redis";
 
 // upload course
 export const uploadCourse = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
@@ -59,7 +60,21 @@ export const editCourse = CatchAsyncError(async (req: Request, res: Response, ne
 // get single course (before purchasing)
 export const getSingleCourse = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const course = await CourseModel.findById(req.params.id).select("-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links")
+        const courseId = req.params.id
+
+        const isCacheExist = await redis.get(courseId)
+
+        if (isCacheExist) {
+            const course = JSON.parse(isCacheExist)
+            return res.status(200).json({
+                success: true,
+                course
+            })
+        }
+
+        const course = await CourseModel.findById(courseId).select("-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links")
+
+        await redis.set(courseId, JSON.stringify(course))
 
         res.status(200).json({
             success: true,
@@ -73,7 +88,17 @@ export const getSingleCourse = CatchAsyncError(async (req: Request, res: Respons
 // get all courses (before purchasing)
 export const getAllCourses = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
     try {
+        const isCacheExist = await redis.get("courses")
+        if (isCacheExist) {
+            const courses = JSON.parse(isCacheExist)
+            return res.status(200).json({
+                success: true,
+                courses
+            })
+        }
         const courses = await CourseModel.find().select("-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links")
+
+        await redis.set("courses", JSON.stringify(courses))
 
         res.status(200).json({
             success: true,
