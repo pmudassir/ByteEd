388be6d313f0d6754ac428a675d9646792e8b641
pmudassir <pmuddasir@gmail.com>
2023-09-26 12:59:52 +0530

    update course

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,8 +1,10 @@
 import express from "express"
+import { editCourse } from "../controllers/course.controller";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 import { createCourse } from "../services/course.services";
 const router = express.Router();
 
-router.post("/create", isAuthenticated, authorizeRoles("admin"), createCourse);
+router.post("/createCourse", isAuthenticated, authorizeRoles("admin"), createCourse);
+router.put("/updateCourse/:id", isAuthenticated, authorizeRoles("admin"), editCourse);
 
 module.exports = router;
