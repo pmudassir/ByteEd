ed584432c47593adcbe9576a3302436b141eccb6
pmudassir <pmuddasir@gmail.com>
2023-09-29 23:16:16 +0530

    working on order v2

--- server/controllers/order.controller.ts
+++ server/controllers/order.controller.ts
@@ -8,25 +8,38 @@ import CourseModel from "../models/Course";
 import path from "path";
 import ejs from "ejs"
 import sendMail from "../utils/sendMail";
+import { newOrder } from "../services/order.services";
 
 // create order
-export const createOrder = CatchAsyncError(async (req: Request, res: Response) => {
+export const createOrder = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { courseId, payment_info } = req.body as IOrder;
 
         const user = await userModel.findById(req.user?._id);
 
         const courseExistInUser = user?.courses.some((course: any) => course.courseId.toString() === courseId.toString())
-        if (courseExistInUser) return new ErrorHandler("You have already purchased this course", 400)
+        if (courseExistInUser) return next(new ErrorHandler("You have already purchased this course", 400))
 
         const course = await CourseModel.findById(courseId)
-        if (!course) return new ErrorHandler("Course not found", 400)
+        if (!course) return next(new ErrorHandler("Course not found", 400))
 
         const data: any = {
             courseId: course._id,
             userId: user?._id,
         }
+        newOrder(data, res, next)
+
+        const mailData = {
+            order: {
+                _id: course._id.slice(0, 6),
+                name: course.name,
+                price: course.price,
+                date: new Date().toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }),
+            }
+        }
+        
+        const html = await ejs.renderFile(path.join(__dirname, "../mails/orderConfirmation.ejs"), mailData)
     } catch (error: any) {
-        return new ErrorHandler(error.message, 500)
+        return next(new ErrorHandler(error.message, 500))
     }
 })
--- server/services/order.services.ts
+++ server/services/order.services.ts
@@ -1,1 +1,9 @@
 import { NextFunction } from "express";
+import { CatchAsyncError } from "../middleware/catchAsyncErrors";
+import OrderModel from "../models/Order";
+
+// create order
+export const newOrder = CatchAsyncError(async (data: any, next: NextFunction) => {
+    const order = await OrderModel.create(data);
+    next(order)
+})
