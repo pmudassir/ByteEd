0aed39034b2ec4f07806e56bf156e86b114861d3
Mudassir Muhammed <96933276+pmudassir@users.noreply.github.com>
2023-12-09 22:56:51 +0530

    working on nav

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 import React, { FC, useState } from "react";
 import Heading from "./utils/Heading";
+import Header from "./components/Header";
 
 interface Props {}
 
 const Page: FC<Props> = (props) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
+
   return (
     <div>
       <Heading
@@ -12,6 +16,7 @@ const Page: FC<Props> = (props) => {
         description="ByteEd is a platform where students will be prepared to face the real world with real skills."
         keywords="Program, Code, Design, Manage, Learn, Share"
       />
+      <Header open={open} setOpen={setOpen} activeItem={activeItem} />
     </div>
   );
 };
